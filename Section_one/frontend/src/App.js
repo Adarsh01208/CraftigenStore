9e88cb780c31dd57fa422840e01aaf9ea27a7243
Your Name <adarshsingh01209@gmail.com>
2023-08-15 18:14:54 +0530

    ..

--- Section_one/frontend/src/App.js
+++ Section_one/frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AddProduct from './components/AddProduct';
+import { UserProvider } from './UserContext';
+import Browse from './components/Browse';
+import ManageUser from './components/ManageUser';
+import ContactUs from './components/ContactUs';
+import UpdateUser from './components/UpdateUser';
+import { Toaster } from 'react-hot-toast';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Toaster position='top-center' />
+      <BrowserRouter>
+        <UserProvider>
+          {/* <Link to="/home">Home Page</Link>
+        <Link to="/login">Login Page</Link>
+        <Link to="/signup"> Signup Page</Link>
+        <Link to="/signin">Signin Page</Link> */}
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Navigate to="/home" />} />
+            <Route path='home' element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='addproduct' element={<AddProduct />} />
+            <Route path='browse' element={<Browse />} />
+            <Route path='contactus' element={<ContactUs />} />
+            <Route path='manageuser' element={<ManageUser />} />
+            <Route path='updateuser/:id' element={<UpdateUser />} />
+          </Routes>
+          <Footer />
+        </UserProvider>
+      </BrowserRouter>
+     
     </div>
   );
 }
