0dc1a4903da2c239985600b855a2543aec9cd75b
Your Name <adarshsingh01209@gmail.com>
2023-08-15 18:15:04 +0530

    ..

---
+++ Section_one/frontend/src/components/dummyData.js
@@ -0,0 +1,128 @@
+const laptopData = [
+    {
+        id: 84473,
+        title: 'King Procession Rectangle',
+        price: 795,
+        reviews: 270,
+        rating: 4.1,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_26aae6eb-1173-46f6-923d-a5dfe57ea666_540x.jpg?v=1651574181'
+    },
+    {
+        id: 84444,
+        title: 'Sui Dhaga Reactangle Tray',
+        price: 595,
+        reviews: 450,
+        rating: 4.6,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/2_2fafcb0e-42ac-410d-8a4e-7a19e582ce02_540x.jpg?v=1679116444'
+    },
+    {
+        id: 82473,
+        title: 'King Procession Laptop Bag',
+        price: 1299,
+        reviews: 290,
+        rating: 4.3,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_77a95405-e6e2-4cf6-99ee-f05c3a04aea0_540x.jpg?v=1649941297'
+    },
+    {
+        id: 67893,
+        title: 'Brass Shubh labh Wall Hanging',
+        price: 860,
+        reviews: 230,
+        rating: 4.4,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_6d0bd4da-17ed-41de-8bab-e0f5e9ff4188_540x.jpg?v=1629210347'
+    },
+    {
+        id: 35578,
+        title: 'Brass Engraved Ganesh Small',
+        price: 1100,
+        reviews: 1230,
+        rating: 4.3,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/2_863a7b92-71a0-495a-b4c7-8f15d8b35072_540x.jpg?v=1655459761'
+    },
+    {
+        id: 32578,
+        title: 'Three Musician Wall Frame',
+        price: 1700,
+        reviews: 130,
+        rating: 4.2,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_2da6771d-9613-49de-8b30-02d61027be6d_540x.jpg?v=1629543030'
+    },
+    {
+        id: 31578,
+        title: 'Brass Engraved Ganesh  2.5 In',
+        price: 695,
+        reviews: 430,
+        rating: 4.2,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/20220810_132234_540x.jpg?v=1663591370'
+    },
+    {
+        id: 31778,
+        title: 'Singal Sardar Iron Wall Frame',
+        price: 825,
+        reviews: 430,
+        rating: 4.2,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_dd1ed6e3-c751-4bd1-823e-366b97ff1933_540x.jpg?v=1628070048'
+    },
+    {
+        id: 21778,
+        title: 'Elephant Wood Art Wall Frame',
+        price: 1250,
+        reviews: 340,
+        rating: 4.7,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_e0c133a1-7e72-4c78-a404-794362575b14_540x.jpg?v=1626460744'
+    },
+    {
+        id: 20778,
+        title: 'Brass StoneWork Aarti Deepak',
+        price: 1600,
+        reviews: 640,
+        rating: 4.5,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_b06725f1-9815-471d-becc-35192eb6e67d_540x.jpg?v=1628866701'
+    },
+    {
+        id: 25678,
+        title: 'Brass Tortoise Aarti Deepak',
+        price: 2900,
+        reviews: 460,
+        rating: 4.5,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_7858460e-7b48-4b9c-b842-ba127d145c35_540x.jpg?v=1628866466'
+    },
+
+    {
+        id: 25678,
+        title: 'T-Lite Holder Kudia CutWork',
+        price: 1100,
+        reviews: 420,
+        rating: 4.3,
+        image: ' https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_34c68fc3-623a-439f-a6b0-abd0c6fb2b37_540x.jpg?v=1649941378'
+    },
+    {
+        id: 20778,
+        title: 'Brass Temple Bell ',
+        price: 595,
+        reviews: 63,
+        rating: 4.5,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/2_c7c0eab1-b8b5-435e-be7f-329ac11401db_540x.jpg?v=1633613632'
+    },
+    {
+        id: 25678,
+        title: 'King Leather Bag',
+        price: 1150,
+        reviews: 430,
+        rating: 4.2,
+        image: 'https://cdn.shopify.com/s/files/1/0555/6256/7874/products/1_1c886575-04e3-4355-9f8c-17173586d112_540x.jpg?v=1626515766'
+    },
+
+    {
+        id: 25678,
+        title: 'Dwaar Coffee Mug Set',
+        price: 600,
+        reviews: 4230,
+        rating: 4.1,
+        image: ' https://cdn.shopify.com/s/files/1/0555/6256/7874/products/A_1f5cb21b-1cb1-4850-b257-0ff9a028d07f_540x.jpg?v=1653470918'
+    },
+
+];
+
+
+export default laptopData;
